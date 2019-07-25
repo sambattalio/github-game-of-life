b1239d8970070115d80fb96cdb2a586b7124f156
sambattalio <sambattalio99@gmail.com>
2019-07-25 00:14:40 -0400

    Switched to param store for persistant variables

--- app.py
+++ app.py
@@ -2,18 +2,45 @@ import os
 from copy import deepcopy
 from ast import literal_eval
 from chalice import Chalice
+import boto3
 
 # init chalice
 app = Chalice(app_name='game-of-life')
 
+# load variables
+def load_params(ssm):
+    params = ssm.get_parameters(Names=['COUNT', 'BOARD', 'ACCESS_TOKEN'])
+
+    BOARD = None
+    COUNT = None
+    ACCESS_TOKEN = None
+
+    for i in params['Parameters']:
+        if i['Name'] == 'COUNT':
+            COUNT = i['Value']
+        elif i['Name'] == 'BOARD':
+            BOARD = i['Value']
+        elif i['Name'] == 'ACCESS_TOKEN':
+            ACCESS_TOKEN = i['Value']
+
+    # set to default values
+    if not COUNT:
+        COUNT = '1'
+    if not BOARD:
+        BOARD = [[-1, -1, -1, -1, -1, -1, -1, -1, -1],
+                   [-1, 0, 0, 0, 0, 0, 0, 0, -1],
+                   [-1, 0, 1, 1, 1, 0, 0, 0, -1],
+                   [-1, 0, 0, 1, 1, 1, 0, 0, -1],
+                   [-1, 0, 0, 0, 0, 0, 0, 0, -1],
+                   [-1, -1, -1, -1, -1, -1, -1, -1, -1]]
+    else:
+        BOARD = literal_eval(BOARD)
+
+    return BOARD, COUNT, ACCESS_TOKEN
+
 ''' GITHUB API '''
 
-# Request head for github
-HEADERS = {
-    'Authorization': 'token {}'.format(os.environ['ACCESS_TOKEN'])
-}
-
-def make_issue(title):
+def make_issue(title, headers):
     ''' make issue with title on base_url '''
     url = os.environ['BASE_URL'] + '/issues'
 
@@ -21,7 +48,7 @@ def make_issue(title):
         'title': title
     }
 
-    resp = requests.request('POST', url, data=json.dumps(data), headers=HEADERS)
+    resp = requests.request('POST', url, data=json.dumps(data), headers=headers)
 
     if resp.status_code == 201:
         print('success')
@@ -29,7 +56,7 @@ def make_issue(title):
         print(resp.content)
         print('failed to create issue')
 
-def close_issues():
+def close_issues(headers):
     ''' Use github api to close all issues '''
     # info for get/patch
     get_all_url = BASE_URL + '/issues'
@@ -38,7 +65,7 @@ def close_issues():
     }
 
     # get all issues
-    resp   = requests.request('GET', get_all_url, headers=HEADERS)
+    resp   = requests.request('GET', get_all_url, headers=headers)
     if 400 < resp.status_code < 500:
         print("Couldn't get issues for repo")
         return
@@ -46,7 +73,7 @@ def close_issues():
 
     # close all issues
     for issue in issues:
-        test = requests.request('PATCH', issue['url'],data=json.dumps(data), headers=HEADERS)
+        test = requests.request('PATCH', issue['url'],data=json.dumps(data), headers=headers)
         if 400 < test.status_code < 500:
             print("Couldn't close right: {}".format(issue['url']))
 
@@ -57,18 +84,6 @@ def make_contributions(x,y):
     close_issues()
 
 ''' GAME OF LIFE '''
-
-# default board for first month's run
-START_BOARD = [[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-               [-1, 0, 0, 0, 0, 0, 0, 0, -1],
-               [-1, 0, 1, 1, 1, 0, 0, 0, -1],
-               [-1, 0, 0, 1, 1, 1, 0, 0, -1],
-               [-1, 0, 0, 0, 0, 0, 0, 0, -1],
-               [-1, -1, -1, -1, -1, -1, -1, -1, -1]]
-
-# load init value for count if not there
-if not os.environ.get('COUNT'):
-    os.environ['COUNT'] = '1'
 
 def check_neighbors(copy, x, y):
     ''' sum points of neighboring blocks '''
@@ -89,13 +104,6 @@ def update_game(board):
             else:
                 board[i][j] = 1
 
-def load_board():
-    ''' load board from env variables '''
-    if not os.environ.get('BOARD'):
-        os.environ['BOARD'] = str(START_BOARD)
-        return START_BOARD
-    return literal_eval(os.environ['BOARD'])
-
 def count_to_index(count):
     ''' convert daily count 1-28 to coordinate '''
     i = (count // (int(os.environ['HEIGHT']) - 2)) + 1
@@ -105,26 +113,35 @@ def count_to_index(count):
         j  = 7
     return i,j
 
-
 ''' MAIN LAMBDA '''
 
-@app.schedule('cron(0 10 * * ? *)')
+#@app.schedule('cron(0 10 * * ? *)')
+@app.schedule('rate(1 minute)')
 def game_handler(event):
     ''' run game of life and create/close issues '''
-    print("Count: " + os.environ['COUNT'])
-    x, y = count_to_index(int(os.environ['COUNT'])) 
+    ssm = boto3.client('ssm', region_name='us-east-1')
+    BOARD, COUNT, ACCESS_TOKEN = load_params(ssm)
+    print("Count: " + COUNT)
+    x, y = count_to_index(int(COUNT)) 
     print(x, y)
-    board = load_board()
-    print(board[x][y])
+    print(BOARD[x][y])
+
+    # Request head for github
+    HEADERS = {
+        'Authorization': 'token {}'.format(ACCESS_TOKEN)
+    }
 
     # Make contributions if 1 for day
     if board[x][y]:
         make_contributions(x, y)
 
     # update count / board if needed
-    os.environ['COUNT'] = str(int(os.environ['COUNT']) + 1)
-    if int(os.environ['COUNT']) > 28:
-        os.environ['COUNT'] = '1'
-        update_game(board)
-        os.environ['BOARD'] = str(board)
+    COUNT = str(int(COUNT) + 1)
+    if int(COUNT) > 28:
+        COUNT = '1'
+        update_game(BOARD)
+        BOARD = str(BOARD)
+        ssm.put_parameter(Name='BOARD', Value=BOARD, Type='String', Overwrite=True)
+    ssm.put_parameter(Name='COUNT', Value=COUNT, Type='String', Overwrite=True)
 
+    return 0
